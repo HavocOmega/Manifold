af96af9f22a4807c7a06da6256786a5d7f0e1f8b
HavocOmega <80192800+HavocOmega@users.noreply.github.com>
2025-06-20 02:29:00 -0600

    feat: menubutton tsx file

--- src/Components/MenuButton.tsx
+++ src/Components/MenuButton.tsx
@@ -1,2 +1,12 @@
 /* --------------------------------- Imports -------------------------------- */
 import React, { StrictMode } from "@rbxts/react";
+
+/* -------------------------------- Interface ------------------------------- */
+interface MenuButtonProps {
+	Name: string;
+	Icon: string;
+	Order: number;
+	OnClick: () => string | void;
+}
+
+/* --------------------------------- Export --------------------------------- */
--- src/Components/app.tsx
+++ src/Components/app.tsx
@@ -1,11 +1,12 @@
 /* --------------------------------- Imports -------------------------------- */
-import { CoreGui } from "@rbxts/services";
 import React, { StrictMode } from "@rbxts/react";
 import { createPortal, createRoot } from "@rbxts/react-roblox";
+import { CoreGui } from "@rbxts/services";
 
 /* ------------------------------- References ------------------------------- */
-const PluginToolbar = plugin.CreateToolbar("Manifold");
-
-/* --------------------------------- Events --------------------------------- */
+const Root = createRoot(new Instance("Folder"));
 
 /* ----------------------------- Initialization ----------------------------- */
+export function render(children: React.ReactNode) {
+	Root.render(<StrictMode>{createPortal(children, CoreGui)}</StrictMode>);
+}
