c9dc1ca999c6435c15e9f73139e9937a5d1a8731
HavocOmega <80192800+HavocOmega@users.noreply.github.com>
2025-06-19 20:42:20 -0600

    wipe

--- src/index.server.ts
+++
@@ -1,7 +0,0 @@
-/* --------------------------------- Imports -------------------------------- */
-
-/* ------------------------------- References ------------------------------- */
-const PluginToolbar: PluginToolbar = plugin.CreateToolbar("Manifold");
-const SettingsButton: PluginToolbarButton = PluginToolbar.CreateButton("Settings", "Open the settings menu", "");
-
-/* ---------------------------------- Props --------------------------------- */
