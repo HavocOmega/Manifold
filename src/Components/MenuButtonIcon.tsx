8a5a9de48a2a682a30eb0648a2ae31c2df375e33
HavocOmega <80192800+HavocOmega@users.noreply.github.com>
2025-06-23 03:15:38 -0600

    feat: menu button icon component

---
+++ src/Components/MenuButtonIcon.tsx
@@ -0,0 +1,39 @@
+/* --------------------------------- Imports -------------------------------- */
+import React from "@rbxts/react";
+
+/* -------------------------------- Interface ------------------------------- */
+interface ComponentProps {
+	Container: Frame | ImageLabel | TextLabel;
+	Icon: string;
+}
+
+/* -------------------------------- Component ------------------------------- */
+function getYSize(Container: Frame | ImageLabel | TextLabel): number {
+	const hasPadding = Container.FindFirstChildOfClass("UIPadding") !== undefined;
+	const ySize = Container.AbsoluteSize.Y;
+
+	if (hasPadding) {
+		const padding = Container.FindFirstChildOfClass("UIPadding") as UIPadding;
+		const offsetPadding = padding.PaddingTop.Offset + padding.PaddingBottom.Offset;
+		const scalePadding = padding.PaddingTop.Scale + padding.PaddingBottom.Scale * ySize;
+
+		return ySize - offsetPadding - scalePadding;
+	}
+
+	return ySize;
+}
+
+export function MenuButtonIcon(Props: ComponentProps): React.Element {
+	const ySize = getYSize(Props.Container);
+
+	return (
+		<imagelabel
+			AnchorPoint={new Vector2(0, 0.5)}
+			BackgroundTransparency={1}
+			Image={Props.Icon}
+			key={"MenuButtonIcon"}
+			Position={UDim2.fromScale(0, 0.5)}
+			Size={new UDim2(0, ySize, 1, 0)}
+		/>
+	);
+}
